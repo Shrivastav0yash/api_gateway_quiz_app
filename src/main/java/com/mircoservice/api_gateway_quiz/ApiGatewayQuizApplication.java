11a65a92986ce25bbdd7d8ef528afe215bda8173
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-06-13 02:48:27 +0530

    API Gateway

---
+++ src/main/java/com/mircoservice/api_gateway_quiz/ApiGatewayQuizApplication.java
@@ -0,0 +1,13 @@
+package com.mircoservice.api_gateway_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayQuizApplication.class, args);
+	}
+
+}
